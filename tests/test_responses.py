2db7c2835ea189ff52ba7048388a61a3c64bc1dc
Adam Cahan <adam@codeplatoon.org>
2024-07-23 15:53:31 -0500

    Fix tests replace assert_ and assertEquals
    - Replace assert_ with assertTrue
    - Replace assertEquals with assertEqual

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -19,21 +19,21 @@ class Test_endpoints(TestCase):
     def test_01_all_classes(self):
         response = self.client.get(reverse("all_subjects"))
         with self.subTest():
-            self.assert_(response.status_code == 200)
+            self.assertTrue(response.status_code == 200)
         content = json.loads(response.content)
-        self.assertEquals(content, all_subjects)
+        self.assertEqual(content, all_subjects)
 
     def test_02_url_pattern_subjects(self):
         url_route = resolve(reverse("all_subjects"))
-        self.assertEquals(url_route.route, 'api/v1/subjects/')
+        self.assertEqual(url_route.route, 'api/v1/subjects/')
 
     def test_03_all_students(self):
         response = self.client.get(reverse("all_students"))
         with self.subTest():
-            self.assert_(response.status_code == 200)
+            self.assertTrue(response.status_code == 200)
         content = json.loads(response.content)
-        self.assertEquals(content, all_students)
+        self.assertEqual(content, all_students)
 
     def test_04_url_pattern_students(self):
         url_route = resolve(reverse("all_students"))
-        self.assertEquals(url_route.route, 'api/v1/students/')
+        self.assertEqual(url_route.route, 'api/v1/students/')
